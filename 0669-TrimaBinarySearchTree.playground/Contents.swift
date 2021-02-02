f879927814d8327d19b7832035339dcb20534e20
troyismad99 <troyhamilton@gmail.com>
2021-02-02 14:16:36 -0800

    Problem 0669 - Trim a Binary Search tree. Print a tree code added

---
+++ 0669-TrimaBinarySearchTree.playground/Contents.swift
@@ -0,0 +1,225 @@
+/*
+ Given the root of a binary search tree and the lowest and highest boundaries as low and high, trim the tree so that all its elements lies in [low, high]. Trimming the tree should not change the relative structure of the elements that will remain in the tree (i.e., any node's descendant should remain a descendant). It can be proven that there is a unique answer.
+
+ Return the root of the trimmed binary search tree. Note that the root may change depending on the given bounds.
+
+Example 1:
+ Input: root = [1,0,2], low = 1, high = 2
+ Output: [1,null,2]
+
+Example 2:
+ Input: root = [3,0,4,null,2,null,null,1], low = 1, high = 3
+ Output: [3,2,null,1]
+
+Example 3:
+ Input: root = [1], low = 1, high = 2
+ Output: [1]
+
+Example 4:
+ Input: root = [1,null,2], low = 1, high = 3
+ Output: [1,null,2]
+
+Example 5:
+ Input: root = [1,null,2], low = 2, high = 4
+ Output: [2]
+
+Constraints:
+     The number of nodes in the tree in the range [1, 104].
+     0 <= Node.val <= 104
+     The value of each node in the tree is unique.
+     root is guaranteed to be a valid binary search tree.
+     0 <= low <= high <= 104
+ */
+
+import Foundation
+
+// Found on stackOverflow
+// https://stackoverflow.com/questions/43898440/how-to-draw-a-binary-tree-in-console/43903427
+// From Alain T (https://stackoverflow.com/users/5237560/alain-t)
+public func treeString<T>(_ node:T, reversed:Bool=false, isTop:Bool=true, using nodeInfo:(T)->(String,T?,T?)) -> String
+{
+   // node value string and sub nodes
+   let (stringValue, leftNode, rightNode) = nodeInfo(node)
+
+   let stringValueWidth  = stringValue.count
+
+   // recurse to sub nodes to obtain line blocks on left and right
+   let leftTextBlock     = leftNode  == nil ? []
+                         : treeString(leftNode!,reversed:reversed,isTop:false,using:nodeInfo)
+                           .components(separatedBy:"\n")
+    
+   let rightTextBlock    = rightNode == nil ? []
+                         : treeString(rightNode!,reversed:reversed,isTop:false,using:nodeInfo)
+                           .components(separatedBy:"\n")
+
+   // count common and maximum number of sub node lines
+   let commonLines       = min(leftTextBlock.count,rightTextBlock.count)
+   let subLevelLines     = max(rightTextBlock.count,leftTextBlock.count)
+
+   // extend lines on shallower side to get same number of lines on both sides
+   let leftSubLines      = leftTextBlock
+                         + Array(repeating:"", count: subLevelLines-leftTextBlock.count)
+   let rightSubLines     = rightTextBlock
+                         + Array(repeating:"", count: subLevelLines-rightTextBlock.count)
+
+   // compute location of value or link bar for all left and right sub nodes
+   //   * left node's value ends at line's width
+   //   * right node's value starts after initial spaces
+   let leftLineWidths    = leftSubLines.map{$0.count}
+   let rightLineIndents  = rightSubLines.map{$0.prefix{$0==" "}.count  }
+
+   // top line value locations, will be used to determine position of current node & link bars
+   let firstLeftWidth    = leftLineWidths.first   ?? 0
+   let firstRightIndent  = rightLineIndents.first ?? 0
+
+
+   // width of sub node link under node value (i.e. with slashes if any)
+   // aims to center link bars under the value if value is wide enough
+   //
+   // ValueLine:    v     vv    vvvvvv   vvvvv
+   // LinkLine:    / \   /  \    /  \     / \
+   //
+   let linkSpacing       = min(stringValueWidth, 2 - stringValueWidth % 2)
+   let leftLinkBar       = leftNode  == nil ? 0 : 1
+   let rightLinkBar      = rightNode == nil ? 0 : 1
+   let minLinkWidth      = leftLinkBar + linkSpacing + rightLinkBar
+   let valueOffset       = (stringValueWidth - linkSpacing) / 2
+
+   // find optimal position for right side top node
+   //   * must allow room for link bars above and between left and right top nodes
+   //   * must not overlap lower level nodes on any given line (allow gap of minSpacing)
+   //   * can be offset to the left if lower subNodes of right node
+   //     have no overlap with subNodes of left node
+   let minSpacing        = 2
+   let rightNodePosition = zip(leftLineWidths,rightLineIndents[0..<commonLines])
+                           .reduce(firstLeftWidth + minLinkWidth)
+                           { max($0, $1.0 + minSpacing + firstRightIndent - $1.1) }
+
+
+   // extend basic link bars (slashes) with underlines to reach left and right
+   // top nodes.
+   //
+   //        vvvvv
+   //       __/ \__
+   //      L       R
+   //
+   let linkExtraWidth    = max(0, rightNodePosition - firstLeftWidth - minLinkWidth )
+   let rightLinkExtra    = linkExtraWidth / 2
+   let leftLinkExtra     = linkExtraWidth - rightLinkExtra
+
+   // build value line taking into account left indent and link bar extension (on left side)
+   let valueIndent       = max(0, firstLeftWidth + leftLinkExtra + leftLinkBar - valueOffset)
+   let valueLine         = String(repeating:" ", count:max(0,valueIndent))
+                         + stringValue
+   let slash             = reversed ? "\\" : "/"
+   let backSlash         = reversed ? "/"  : "\\"
+   let uLine             = reversed ? "¯"  : "_"
+   // build left side of link line
+   let leftLink          = leftNode == nil ? ""
+                         : String(repeating: " ", count:firstLeftWidth)
+                         + String(repeating: uLine, count:leftLinkExtra)
+                         + slash
+
+   // build right side of link line (includes blank spaces under top node value)
+   let rightLinkOffset   = linkSpacing + valueOffset * (1 - leftLinkBar)
+   let rightLink         = rightNode == nil ? ""
+                         : String(repeating:  " ", count:rightLinkOffset)
+                         + backSlash
+                         + String(repeating:  uLine, count:rightLinkExtra)
+
+   // full link line (will be empty if there are no sub nodes)
+   let linkLine          = leftLink + rightLink
+
+   // will need to offset left side lines if right side sub nodes extend beyond left margin
+   // can happen if left subtree is shorter (in height) than right side subtree
+   let leftIndentWidth   = max(0,firstRightIndent - rightNodePosition)
+   let leftIndent        = String(repeating:" ", count:leftIndentWidth)
+   let indentedLeftLines = leftSubLines.map{ $0.isEmpty ? $0 : (leftIndent + $0) }
+
+   // compute distance between left and right sublines based on their value position
+   // can be negative if leading spaces need to be removed from right side
+   let mergeOffsets      = indentedLeftLines
+                           .map{$0.count}
+                           .map{leftIndentWidth + rightNodePosition - firstRightIndent - $0 }
+                           .enumerated()
+                           .map{ rightSubLines[$0].isEmpty ? 0  : $1 }
+
+
+   // combine left and right lines using computed offsets
+   //   * indented left sub lines
+   //   * spaces between left and right lines
+   //   * right sub line with extra leading blanks removed.
+   let mergedSubLines    = zip(mergeOffsets.enumerated(),indentedLeftLines)
+                           .map{ ( $0.0, $0.1, $1 + String(repeating:" ", count:max(0,$0.1)) ) }
+                           .map{ $2 + String(rightSubLines[$0].dropFirst(max(0,-$1))) }
+
+   // Assemble final result combining
+   //  * node value string
+   //  * link line (if any)
+   //  * merged lines from left and right sub trees (if any)
+   let treeLines = [leftIndent + valueLine]
+                 + (linkLine.isEmpty ? [] : [leftIndent + linkLine])
+                 + mergedSubLines
+
+   return (reversed && isTop ? treeLines.reversed(): treeLines)
+          .joined(separator:"\n")
+}
+
+// required for the treestring
+extension TreeNode
+{
+   var asString:String { return treeString(self){("\($0.val)",$0.left,$0.right)}  }
+}
+
+
+
+// Definition for a binary tree node.
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+func trimBST(_ root: TreeNode?, _ low: Int, _ high: Int) -> TreeNode? {
+
+    
+    return root
+}
+
+
+
+let tree1: TreeNode = TreeNode.init(1)
+tree1.left = TreeNode.init(0)
+tree1.right = TreeNode.init(2)
+print(tree1.asString)
+print(trimBST(tree1, 1, 2)!.asString)
+
+let tree2: TreeNode = TreeNode.init(3)
+tree2.left = TreeNode.init(0)
+tree2.right = TreeNode.init(4)
+//tree2.left?.left = TreeNode.init()
+tree2.left?.right = TreeNode.init(2)
+tree2.left?.right?.left = TreeNode.init(1)
+print(tree2.asString)
+print(trimBST(tree2, 1, 3)!.asString)
+
+let tree3: TreeNode = TreeNode.init(1)
+print(tree3.asString)
+print(trimBST(tree3, 1, 2)!.asString)
+
+let tree4: TreeNode = TreeNode.init(1)
+tree4.right = TreeNode.init(2)
+print(tree4.asString)
+print(trimBST(tree4, 1, 3)!.asString)
+
+let tree5: TreeNode = TreeNode.init(1)
+tree5.right = TreeNode.init(2)
+print(tree5.asString)
+print(trimBST(tree5, 2, 4)!.asString)
